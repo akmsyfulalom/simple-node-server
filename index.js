3eca62a650fb73fba03950e6a7a1202cce73e56a
akmsyfulalom <akmsyfulbd12@gmail.com>
2022-11-13 05:17:32 +0600

    MongoDB CRUD operation

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ async function run() {
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await userCollection.insertOne(user)
-            console.log(result);
             user.id = result.insertedId
             res.send(user)
         })
